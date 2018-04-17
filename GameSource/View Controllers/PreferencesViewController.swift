480a8df4fbd6a9c7b82bbe6b8e931a9f8eaff55a
Ruben A Gonzalez <Ruben@MacBook-Pro-9.local>
2018-04-16 20:20:13 -0700

    created storyboard

---
+++ GameSource/View Controllers/PreferencesViewController.swift
@@ -0,0 +1,93 @@
+//
+//  PreferencesViewController.swift
+//  GameSource
+//
+//  Created by Ruben A Gonzalez on 4/13/18.
+//  Copyright © 2018 Langtian Qin. All rights reserved.
+//
+
+import UIKit
+import Parse
+
+class PreferencesViewController: UIViewController {
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+    /* TODO */
+    @IBAction func tapAction(_ sender: Any) {
+        
+        // If user likeAction is not true, select it and assign true to it
+        
+        // else deselect it and assign false
+    }
+    
+    /* TODO */
+    @IBAction func tapAdventure(_ sender: Any) {
+        
+        // If user likeAdventure is not true, select it and assign true to it
+        
+        // else deselect it and assign false
+    }
+    
+    @IBAction func tapRPG(_ sender: Any) {
+        
+        // If user likeRPG is not true, select it and assign true to it
+        
+        // else deselect it and assign false
+    }
+    
+    @IBAction func tapHorror(_ sender: Any) {
+        
+        // If user likeHorror is not true, select it and assign true to it
+        
+        // else deselect it and assign false
+    }
+    
+    @IBAction func tapSport(_ sender: Any) {
+        
+        // If user likeSport is not true, select it and assign true to it
+        
+        // else deselect it and assign false
+    }
+    
+    @IBAction func tapStrategy(_ sender: Any) {
+        
+        // If user likeStrategy is not true, select it and assign true to it
+        
+        // else deselect it and assign false
+    }
+    
+    @IBAction func tapCasual(_ sender: Any) {
+        
+        // If user likeCasual is not true, select it and assign true to it
+        
+        // else deselect it and assign false
+    }
+    
+    @IBAction func tapMMO(_ sender: Any) {
+        
+        // If user likeMMO is not true, select it and assign true to it
+        
+        // else deselect it and assign false
+    }
+    
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
---
+++ Podfile
@@ -0,0 +1,22 @@
+# Uncomment the next line to define a global platform for your project
+# platform :ios, '9.0'
+
+target 'GameSource' do
+  # Comment the next line if you're not using Swift and don't want to use dynamic frameworks
+  use_frameworks!
+
+  # Pods for GameSource
+  pod 'Parse'
+  pod 'FirebaseUI'
+
+  target 'GameSourceTests' do
+    inherit! :search_paths
+    # Pods for testing
+  end
+
+  target 'GameSourceUITests' do
+    inherit! :search_paths
+    # Pods for testing
+  end
+
+end
